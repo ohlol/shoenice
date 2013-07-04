6dea1bc590bfc27bc3b67ece7d1e00c74fa47c4c
Scott Smith <scott@boundary.com>
2013-07-03 23:01:40 -0700

    finish initial spec

--- shoenice.go
+++ shoenice.go
@@ -3,30 +3,59 @@ package shoenice
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ohlol/graphite-go"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type StatsInstance struct {
 	Counters map[string]int `json:"counters"`
-	Gauges map[string]interface{} `json:"gauges"`
+	Gauges map[string]int `json:"gauges"`
 }
 
 func NewStatsInstance() *StatsInstance {
 	return &StatsInstance{
 		Counters: make(map[string]int),
-		Gauges: make(map[string]interface{}),
+		Gauges: make(map[string]int),
 	}
 }
 
-func (si *StatsInstance) Incr(stat string, val int) {
+func (si *StatsInstance) Incr(stat string) {
+	si.Counters[stat]++
+}
+
+func (si *StatsInstance) IncrN(stat string, val int) {
 	si.Counters[stat] += val
 }
 
-func (si *StatsInstance) Gauge(stat string, val interface{}) {
+func (si *StatsInstance) Gauge(stat string, val int) {
 	si.Gauges[stat] = val
 }
 
-func (si *StatsInstance) RunServer(listenaddr string) {
+func (si *StatsInstance) fmtGraphite(prefix string) []graphite.Metric {
+	var (
+		key string
+		metrics []graphite.Metric
+	)
+
+	now := time.Now().Unix()
+
+	for stat, val := range si.Counters {
+		key = strings.Join([]string{prefix, "counters", stat}, ".")
+		metrics = append(metrics, graphite.Metric{Name: key, Value: strconv.Itoa(val), Timestamp: now})
+	}
+
+	for stat, val := range si.Gauges {
+		key = strings.Join([]string{prefix, "gauges", stat}, ".")
+		metrics = append(metrics, graphite.Metric{Name: key, Value: strconv.Itoa(val), Timestamp: now})
+	}
+
+	return metrics
+}
+
+func (si *StatsInstance) RunServer(listenaddr string, prefix string, sendInterval int, graphiteAddr string, graphitePort uint16) {
 	statsHandlerFunc := func(w http.ResponseWriter, r *http.Request) {
 		encoded, _ := json.Marshal(si)
 
@@ -37,5 +66,13 @@ func (si *StatsInstance) RunServer(listenaddr string) {
 	go func() {
 		http.HandleFunc("/stats", statsHandlerFunc)
 		http.ListenAndServe(listenaddr, nil)
+	}()
+
+	go func() {
+		graphite := graphite.Connect(graphite.GraphiteServer{Host: graphiteAddr, Port: graphitePort})
+		for {
+			graphite.Sendall(si.fmtGraphite(prefix))
+			time.Sleep(time.Duration(sendInterval) * time.Second)
+		}
 	}()
 }
